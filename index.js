829cf863e998d222686567f291649b60364d84ec
Pradhap-devs <pradhapelumalai45@gmail.com>
2025-12-14 08:45:00 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+// Active navbar link on scroll
+const sections = document.querySelectorAll("section, main");
+const navLinks = document.querySelectorAll("nav ul li a");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop - 120;
+    if (scrollY >= sectionTop) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach(link => {
+    link.classList.remove("active");
+    if (link.getAttribute("href") === "#" + current) {
+      link.classList.add("active");
+    }
+  });
+});
